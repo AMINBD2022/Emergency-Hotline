f76d7da77f1c73e51a3656bbfd71cf3f9bb633a8
aminbd2022 <aminulxrpd@gmail.com>
2025-08-27 10:46:54 +0600

    updated some title

--- app/app.js
+++ app/app.js
@@ -9,3 +9,34 @@ for (let heart of hearts){
         document.getElementById("heart-number-count").innerText = heartNumberCount;
     })
 }
+
+// hoistory delate button 
+
+let delateHistory = document.getElementById("history-delate").addEventListener("click", function(){
+    let hestoryContainer = document.getElementById("hestory-container");
+    hestoryContainer.remove(hestoryContainer);
+})
+
+
+
+
+// Calling Function 
+
+let availeAbleCoins = parseInt(document.getElementById("available-coins").innerText);
+let callBtns = document.getElementsByClassName("call-btn");
+for (let callBtn of callBtns){
+    callBtn.addEventListener("click", function(){
+        if( availeAbleCoins >= 20){
+            availeAbleCoins = availeAbleCoins -20;
+            document.getElementById("available-coins").innerText = availeAbleCoins;
+
+        }else{
+            alert("you don't have enougth Coin to call")
+        }
+        
+        let nationals = document.getElementsByClassName("national");
+        for(let national of nationals){
+            alert(national.innerText)
+        }
+    })
+}
